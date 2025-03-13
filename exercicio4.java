3cdcb2c099655e2659a8213d69251f2dc8ffc61c
Deivid Barreto <barreto.reis@hotmail.com>
2025-03-13 10:14:48 -0300

    testando com char

--- exercicio4.java
+++ exercicio4.java
@@ -1,13 +1,15 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class exercicio4 {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double x;
+		char x;
 		
-		x = sc.nextDouble();
+		x = sc.next().charAt(0);
 		
 		
 		System.out.println("Voce digitou: " + x);
