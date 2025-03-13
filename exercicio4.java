f28206c030e74c1446cfbe1a164c3a23d81dbb2c
Deivid Barreto <barreto.reis@hotmail.com>
2025-03-13 10:21:23 -0300

    entrada de dados  pt2

--- exercicio4.java
+++ exercicio4.java
@@ -6,22 +6,18 @@ public class exercicio4 {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		String x;
-		int y;
-		double z;
-		
-		x = sc.next();
-		y = sc.nextInt();
-		z = sc.nextDouble();
-		
+
+		String s1, s2, s3;
+
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+
 		System.out.println("Dados digitados: ");
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		
-		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
 		sc.close();
 	}
 
